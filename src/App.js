ae07eb50afbb803bef8eae4791b6ab7c3fb8141b
Amitesh Anand <aamitesh7@gmail.com>
2023-08-05 01:53:51 +0530

    webpack server setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import "./App.css"
 
 const App = () => {
-    <div className="App"> <h1> Hello React</h1>
-    </div>
+    return (
+        <div className="App">
+            <h1> Hello React</h1>
+        </div>
+    );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Depricated in React18
+// ReactDOM.render(<App/> ,document.getElementById("root"));
 
