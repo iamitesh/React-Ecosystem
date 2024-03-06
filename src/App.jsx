9c8257aa33eb063c9826849f1c60331e8f863c80
Amitesh Anand <aamitesh7@gmail.com>
2024-03-06 21:23:45 +0530

    add react routing and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
-
 import './App.css'
 import Todo from './components/Todo/Todo';
+import { Routes, Route } from 'react-router-dom'
 
+import Navbar from "./components/Navbar/Navbar.jsx"
+import Stopwatch from './components/Stopwatch/Stopwatch.jsx';
+import DigitalClock from './components/DigitalClock/DigitalClock.jsx';
 function App() {
-  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <>
-      <Todo />
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Todo/>} />
+        <Route path='/Stopwatch' element={<Stopwatch/>} />
+        <Route path='/DigitalClock' element={<DigitalClock/>} />
+      </Routes>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+import './Navbar.css'
+const Navbar= () =>{
+  return (
+  <div className='navbar'>
+    <li className='list-item'>
+      <Link to="/">Todo</Link>
+    </li>
+    <li className='list-item'>
+      <Link to="/Stopwatch">Stopwatch</Link>
+    </li>
+    <li className='list-item'>
+      <Link to="/DigitalClock">Digital Clock</Link>
+    </li>
+    <li className='list-item'>
+      <Link to="/MovieSearch">Movie Search</Link>
+    </li>
+    <li className='list-item'>
+      <Link to="/FileUploader">File Uploader</Link>
+    </li>
+    <li className='list-item'>
+      <Link to="/ChatApp">Chat App</Link>
+    </li>
+  </div>
+  );
+}
+export default Navbar;
