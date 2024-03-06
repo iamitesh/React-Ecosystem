146076c71c81e22d158394c8c04748a1bf601ff3
Amitesh Anand <aamitesh7@gmail.com>
2024-03-06 21:20:36 +0530

    add Digital Clock and Stopwatch component

---
+++ src/components/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './DigitalClock.css'
+
+function DigitalClock() {
+  return (
+    <>
+      <h2>DigitalClock</h2>
+    </>
+  )
+}
+
+export default DigitalClock;
---
+++ src/components/Stopwatch/Stopwatch.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './Stopwatch.css'
+
+function Stopwatch() {
+  return (
+    <>
+      <h2>Stopwatch</h2>
+    </>
+  )
+}
+
+export default Stopwatch;
