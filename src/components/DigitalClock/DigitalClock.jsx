c9edbdbba7fdfc931cdebe2781f2d46315165138
Amitesh Anand <aamitesh7@gmail.com>
2024-03-08 03:26:02 +0530

    Implement Digital Clock

--- src/components/DigitalClock/DigitalClock.jsx
+++ src/components/DigitalClock/DigitalClock.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 import './DigitalClock.css'
 
 function DigitalClock() {
+  let currTime = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(currTime);
+  const updateTime = () => {
+    let currTime = new Date().toLocaleTimeString();
+    setTime(currTime)
+  }
+  setInterval(updateTime, 1000)
   return (
-    <>
-      <h2>DigitalClock</h2>
-    </>
+    <h2 className='digital-timer'>{time}</h2>
   )
 }
 
